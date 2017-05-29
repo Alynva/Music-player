6834b4765be73c736a6beb213ca08a084727c4f0
Alisson Nunes <alynva@gmail.com>
2017-05-29 09:19:10 -0300

    Adicionado: agumentos da main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include "MusicPlayer.h"
 
-int main(int argv, char** args) {
+int main(int argc, char** argv) {
 	
 	MusicPlayer player;
 	player.init();
 	
-	player.addMusic("../media/songs/Detonautas_-_O_dia_que_nao_terminou.ogg");
-	player.addMusic("../media/songs/Los_Hermanos_-_Anna_Julia.ogg");
-	player.addMusic("../media/songs/Marcelo_D2_-_Desabafo_Desabafo.ogg");
-	player.addMusic("../media/songs/I_heard_it_through_the_grapevine_-_Creedence.ogg");
+	if (argc == 1) {
+		player.addMusic("../media/songs/Detonautas_-_O_dia_que_nao_terminou.ogg");
+		player.addMusic("../media/songs/Los_Hermanos_-_Anna_Julia.ogg");
+		player.addMusic("../media/songs/Marcelo_D2_-_Desabafo_Desabafo.ogg");
+		player.addMusic("../media/songs/I_heard_it_through_the_grapevine_-_Creedence.ogg");
+	} else
+		for (int i = 1; i < argc; i++)
+			player.addMusic(argv[i]);
 	
 	player.play();
 	
